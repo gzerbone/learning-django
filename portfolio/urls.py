6047a9833c5b7e63d17350bd9a01013f677cba64
Gabriela Zerbone <gzmbaptista.cic@uesc.br>
2024-01-14 18:39:08 -0300

    add random description

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+
 urlpatterns = [
-    path('', views.home),
-    path('project/<int:id>/', views.projects),
+    path('', views.home, name="portfolio-home"),
+    # id dentro de <int:id> vai passar como parametro na função projects
+    path('project/<int:id>/', views.projects, name="portfolio-projeto"), 
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from utils.portfolio.factory import make_portfolio
+
 
 def home(request):
-    return render(request, 'portfolio/pages/home.html')
+    return render(request, 'portfolio/pages/home.html', context={
+        'portfolios': [make_portfolio() for var in range(4)], 
+    })
 
 
 def projects(request, id):
-    return render(request, 'portfolio/pages/projects_view.html')
+    return render(request, 'portfolio/pages/projects_view.html', context={
+        'portfolio': make_portfolio(),
+        'is_detail_page': True, #Se a pagina for a pagina de um projeto o botão de "ver mais não vai existir"
+        })
