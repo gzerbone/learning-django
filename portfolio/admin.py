1277e8112c48b2d0562f7046cba1ff8e63cf8fd9
Gabriela Zerbone <gzmbaptista.cic@uesc.br>
2024-01-23 16:24:03 -0300

    adding portfolio admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Category
+
+from .models import Category, Portfolio  # importando Models
 
 
-
+# primeira maneira de registrar uma model no admin
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 admin.site.register(Category, CategoryAdmin)
+
+# Segunda maneira de registrar uma model no admin
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    ...
